4504f7d338378a588b359fd1b468c4166968fd99
Nassim Benmalek <nbenmale@amazon.fr>
2024-09-30 11:10:05 +0200

    chore: clean data resource configuration

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { data } from './data/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
-  data,
 });
